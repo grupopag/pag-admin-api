df46de67d9513dd305301d77663691f152693148
gustavo <>
2025-02-01 02:03:23 -0300

    ok

--- src/main/java/com/grupo/pag/pagadminapi/database/repository/UsuarioRepository.java
+++ src/main/java/com/grupo/pag/pagadminapi/database/repository/UsuarioRepository.java
@@ -3,7 +3,7 @@ package com.grupo.pag.pagadminapi.database.repository;
 import com.grupo.pag.pagadminapi.database.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Usuario findByEmail(String email);
 
--- src/main/java/com/grupo/pag/pagadminapi/services/TokenService.java
+++ src/main/java/com/grupo/pag/pagadminapi/services/TokenService.java
@@ -1,7 +1,9 @@
 package com.grupo.pag.pagadminapi.services;
 
 import com.grupo.pag.pagadminapi.database.entities.TokenUsuario;
+import com.grupo.pag.pagadminapi.database.entities.Usuario;
 import com.grupo.pag.pagadminapi.database.repository.TokenUsuarioRepository;
+import com.grupo.pag.pagadminapi.database.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,14 @@ import org.springframework.stereotype.Service;
 public class TokenService {
 
     private final TokenUsuarioRepository tokenUsuarioRepository;
-
+    private final UsuarioRepository usuarioRepository;
     public TokenUsuario findTokenByToken(String token) {
         TokenUsuario tokenUsuario = tokenUsuarioRepository.findByToken(token);
+        Usuario usuario = usuarioRepository.findById(tokenUsuario.getUsuario().getUsuarioId()).get();
+
+        usuario.setAtivo(true);
+        usuarioRepository.save(usuario);
+
         if (tokenUsuario != null) {
           tokenUsuario.setUtilizado(true);
           return tokenUsuarioRepository.save(tokenUsuario);
